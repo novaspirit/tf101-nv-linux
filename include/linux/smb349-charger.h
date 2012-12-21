a6a45c156c7904dce93082b3f276054a3c3a2133
novaspirit <x3maniac@gmail.com>
2012-12-21 14:39:30 -0500

    initial commit: working kernel

---
+++ include/linux/smb349-charger.h
@@ -0,0 +1,59 @@
+/*
+ * include/linux/smb349-charger.h
+ *
+ * Battery charger driver interface for Summit SMB349
+ *
+ * Copyright (C) 2012 NVIDIA Corporation
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ */
+
+#ifndef __LINUX_SMB349_CHARGER_H
+#define __LINUX_SMB349_CHARGER_H
+
+#include <linux/regulator/machine.h>
+
+enum charging_states {
+	idle,
+	progress,
+	completed,
+	stopped,
+};
+
+enum charger_type {
+	AC,
+	USB,
+};
+
+typedef void (*charging_callback_t)(enum charging_states state,
+enum charger_type chrg_type, void *args);
+
+struct smb349_charger {
+	struct i2c_client	*client;
+	struct device	*dev;
+	void	*charger_cb_data;
+	enum charging_states state;
+	enum charger_type chrg_type;
+	charging_callback_t	charger_cb;
+};
+
+int smb349_battery_online(void);
+/*
+ * Register callback function for the client.
+ * Used by fuel-gauge driver to get battery charging properties.
+ */
+extern int register_callback(charging_callback_t cb, void *args);
+
+#endif /*__LINUX_SMB349_CHARGER_H */
